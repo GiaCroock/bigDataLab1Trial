9cc0b90ebbe64f7ec3147a6d1adf6f138366ebe1
raelware <2153459@students.wits.ac.za>
2022-03-27 12:58:45 +0200

    elementwise

--- 2dpthread.c
+++ 2dpthread.c
@@ -12,30 +12,30 @@ pthread_mutex_t lock;
 int A[max_dimensions][max_dimensions];
 int B[max_dimensions][max_dimensions];
 int C[max_dimensions][max_dimensions];
-int counter = 0;
+int counter=0;
 
 void *rank2pthread_row(void *arg);
 void initialise_matrices(int N);
 void print_2D_matrix(int matrix[max_dimensions][max_dimensions], int N);
 double time_for_round(double start, double stop);
 void pthread_2d_multiply_sequence(int nthreads, int N);
-double standard_deviation(double mean, double data[1000]) ;
+double standard_deviation(double mean, double data[100]) ;
 
 int main(int argc, char *argv[])
 {
 
-      for (int dimension = 10; dimension <= 50; dimension = dimension + 10)
+      for (int dimension = 10; dimension <= 30; dimension = dimension + 10)
       {
 
             for (int thread_request = 8; thread_request >= 2; thread_request = thread_request / 2)
             {
-                  counter = 0;
+                  counter=0;
                   double total_time = 0;
                   int N = dimension; // this is the dimensions of the matrix for the round
                   int nthreads = thread_request;
                   double start = 0;
                   double stop = 0;
-                  int samples = 1000;
+                  int samples = 100;
                   double mean = 0;
                   double time_data[samples];
                   double ind_times = 0;
@@ -44,7 +44,8 @@ int main(int argc, char *argv[])
                   // repeat 1000 times so can take an average
                   for (int loops = 0; loops < samples; loops++)
                   {
-                        counter = 0;
+                   
+                        counter=0;
                         srand(time(NULL));
 
                         initialise_matrices(N);
@@ -109,21 +110,26 @@ void initialise_matrices(int N)
 }
 
 void *rank2pthread_row(void *arg)
-{ // a thread running this function will be responsible for calculating row i (i=counter) of the resultant matrix
+{ // a thread running this function will be responsible for incrementing rows till all elements in row
+//
       int N = *((int *)arg);
+      int i=0; //because imediately increment on line 119;
+      //should be -1 but then get segmentation
+      int j=0;
       pthread_mutex_lock(&lock);
-      int i = counter++; // ensures every thread that runs this thunk calculates a NEW row (counter is global)
+      if((counter++)%N==0){j=0, i++;}
+      else {j++;}
       pthread_mutex_unlock(&lock);
-      for (int j = 0; j < N; j++)
             for (int k = 0; k < N; k++)
                   C[i][j] += A[i][k] * B[k][j];
+                  
 }
 
 void pthread_2d_multiply_sequence(int nthreads, int N)
 {
       pthread_t threads[nthreads];
 
-      int loopNum = (N / nthreads); // interger division of the total number of rows divided by the no of created threads
+      int loopNum = ((N*N) / nthreads); // interger division of the total number of elements divided by the no of created threads
 
       for (int j = 0; j < loopNum; j++)
       {
@@ -139,8 +145,8 @@ void pthread_2d_multiply_sequence(int nthreads, int N)
             }
       }
 
-      // remainder of rows to be computetd
-      int loopNum2 = N % nthreads;
+      // remainder of elements to be computetd
+      int loopNum2 = (N*N) % nthreads;
 
       for (int i = 0; i < loopNum2; i++)
       {
